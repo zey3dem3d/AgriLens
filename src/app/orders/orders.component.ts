b0ce8667e3b43114a93bec4f395ac6ad06ea62fe
zey3dem3d <zeyademad987@gmail.com>
2025-05-05 08:45:51 +0300

    Order edited (Add 2 way data binding) and fixed navbar issue

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+  styleUrl: './orders.component.css',
 })
 export class OrdersComponent {
+  formGroup!: FormGroup;
+  floors: number = 0;
+  cells: number = 0;
+  sensors: string = 'DHT11, Camera, Movement system';
 
+  submitForm() {
+    // console.log(this.formGroup);
+  }
 }
